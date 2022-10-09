214769f815f9b97d5e8674ab9c2c27896b8c41a4
jlenczewska <julia.lenczewska@britenet.com.pl>
2022-10-09 17:34:28 +0200

    delete files

--- force-app/main/default/aura/campingListItem/campingListItemController.js
+++
@@ -1,9 +0,0 @@
-({
-	 packItem: function(component, event, helper) {
-        var cItem = component.get("v.item",true);
-        cItem.Packed__c = true;
-        component.set("v.item",cItem);
-       
-        event.getSource().set("v.disabled","true");
-    }
-})
