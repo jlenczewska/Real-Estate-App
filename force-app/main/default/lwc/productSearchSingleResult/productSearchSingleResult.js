cc0c74a1a85d329a0d6dc2bd197f6dd3a611ac45
jlenczewska <julia.lenczewska@britenet.com.pl>
2022-11-22 21:51:00 +0100

    push changes

--- force-app/main/default/lwc/productSearchSingleResult/productSearchSingleResult.js
+++ force-app/main/default/lwc/productSearchSingleResult/productSearchSingleResult.js
@@ -16,11 +16,6 @@ export default class ProductSearchSingleResult extends NavigationMixin(
     RE_Parking_spaces
   };
 
-connectedCallback(){
-  console.log(this.product.DisplayUrl.replace('https://britenet28-dev-ed.file.force.com/', ''));
-
-}
-
   redirectToProductPag() {
     this[NavigationMixin.GenerateUrl]({
       type: "standard__recordPage",
